40c1466476866886c57b4045257e0249c8ae79e5
Alicite <lunacarter1503@gmail.com>
2026-04-16 21:15:44 -0300

    Refactor question display logic and improve answer feedback mechanism

--- script.js
+++ script.js
@@ -25,39 +25,52 @@ const fetchTradutor = async (texto) => {
 const mostrarQuestoes = async () => {
     const container = document.querySelector('#question-container');
     const questions = await fetchTrivia();
-    container.innerHTML = '';
-
-    questions.forEach(async (question) => {
+    let acertos = 0;
+    
+    for (const question of questions){
+        container.innerHTML = '';
         const novaQuestao = document.createElement('div');
-        novaQuestao.classList.add('question');
-        
-        const respostas = [...question.incorrect_answers, question.correct_answer];
-        respostas.sort((a, b) => {
-            if (Math.random() < 0.5){
-                return a - b
-            } else {
-                return b - a
-            }
-        })
         novaQuestao.innerHTML = decodeURIComponent(await fetchTradutor(question.question)) + '<br>';
-
-        respostas.forEach(async (resposta) => {
+        container.appendChild(novaQuestao);
+        
+        const divResposta = document.createElement('div');        
+        const respostas = [...question.incorrect_answers, question.correct_answer];
+        respostas.sort((a, b) => Math.random() - 0.5);
+    
+        for (const resposta of respostas){
             const botaoResposta = document.createElement('button');
             botaoResposta.innerText = decodeURIComponent(await fetchTradutor(resposta));
-
-            botaoResposta.onclick = async () => {
-                if (botaoResposta.innerText == await fetchTradutor(question.correct_answer)){
-                    botaoResposta.style.backgroundColor = 'green';
-                } else {
-                    botaoResposta.style.backgroundColor = 'red';
+            divResposta.appendChild(botaoResposta);
+        }
+    
+        container.appendChild(divResposta);
+    
+        const respostaTraduzida = decodeURIComponent(await fetchTradutor(question.correct_answer));
+    
+        await new Promise((resolve) => {
+            const botoesResposta = document.querySelectorAll('button');
+            botoesResposta.forEach((botao) => {
+                botao.onclick = () => {
+                    if (botao.innerText == respostaTraduzida){
+                        botao.style.backgroundColor = 'green';
+                        acertos++;
+                    } else {
+                        botao.style.backgroundColor = 'red';
+                        botoesResposta.forEach((bt) => {
+                            if (bt.innerText == respostaTraduzida){
+                                bt.style.backgroundColor = 'green';
+                            }
+                        });
+                    }
+                    setTimeout(() => {
+                        resolve();
+                    }, 1000)
                 }
-            }
-
-            novaQuestao.appendChild(botaoResposta);
+            });
         })
-        
-        container.appendChild(novaQuestao);
-    });
+    }
+
+    container.innerHTML = `Você acertou ${acertos}/5 questões!`
 }
 
 mostrarQuestoes();
